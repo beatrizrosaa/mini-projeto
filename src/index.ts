2d838644d39e8b33b1d1438a5b828ac7a81152d8
beatrizrosa <rosabeatriz438@yahoo.com.br>
2025-10-19 00:06:44 -0300

    Adiciona e configura o CORS

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import express from 'express';
 import 'dotenv/config';
 import connectDB from './database';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware para parsear o corpo das requisições como JSON
 app.use(express.json());
-
+app.use(cors());
 // Conectar ao banco de dados
 connectDB();
 
